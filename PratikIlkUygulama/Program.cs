ae024aca89c141b729f541a10fcee660d9b97148
sefayilmaz <sefaylmaz14@gmail.com>
2024-07-18 14:14:39 +0300

    Add project files.

---
+++ PratikIlkUygulama/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Sefa Yılmaz");
+Console.WriteLine("Uzun yıllar farklı bir iş alanında çalışmalarımdan sonra tanışma fırsatı bulduğum ve ilgimi çeken yazılım dünyasına, profesyonel yardım alarak emin adımalarla girmek istedim. Bu sebeple yaptığım araştırmalar sonucu Patika ile yola devam etme kararı aldım");
+
